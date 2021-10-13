de42ce2a4d7c4cc9c45960c3aadf90e2db4a2333
willinggang <willinggang@gmail.com>
2021-10-13 22:48:35 +0800

    第一次提交

---
+++ src/test/java/com/farmer/sunjia/SunJiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmer.sunjia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunJiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
