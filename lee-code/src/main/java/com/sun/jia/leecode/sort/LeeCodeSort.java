c2dfb58f654a7206660096a5d310a36f7f1f60ca
willinggang <willinggang@gmail.com>
2021-11-04 21:42:43 +0800

    有序数组查找目标下标

--- lee-code/src/main/java/com/sun/jia/leecode/sort/LeeCodeSort.java
+++ lee-code/src/main/java/com/sun/jia/leecode/sort/LeeCodeSort.java
@@ -48,4 +48,22 @@ public class LeeCodeSort {
         nums[right] = temp;
     }
 
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0;
+        int r = numbers.length-1;
+        while (l<=r){
+            if (numbers[l]+numbers[r]== target){
+                return new int[]{l+1,r+1};
+            }
+            if (numbers[l]+numbers[r]>target){
+                r--;
+            }
+            if (numbers[l]+numbers[r]<target){
+                l++;
+            }
+        }
+
+        return new int[]{-1,-1};
+    }
+
 }
